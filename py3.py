0510c26b90802927c421dd1ac0544aed3828f76a
quanvqg <94589235+quanvqg@users.noreply.github.com>
2021-11-28 09:45:43 +0700

    Add files via upload

--- py3.py
+++ py3.py
@@ -6,11 +6,11 @@
 # Created date: 13/11/2021
 # Last modified date: 24/11/2021
 
-# import the math function
+# Import the math function
 import math
 
 
-# define flour_order function
+# Define flour_order function
 def flour_order(large_thick, large_thin, medium_thick, medium_thin):
     """
     Decide between provider A and B based on amount of flour needed
@@ -20,29 +20,31 @@ def flour_order(large_thick, large_thin, medium_thick, medium_thin):
     :param medium_thin:
     :return:integer, string, integer
     """
-
+    # Call and make all value to integer
     total_flour = int(flour_amount(large_thick, large_thin, medium_thick, medium_thin))
     money_on_a = int(money_spent_on_a(total_flour))
     money_on_b = int(money_spent_on_b(total_flour))
+    # Print out the result of the function
     print('We need to order ' + str(total_flour) + ' kg of flour, which costs ' + str(
         money_on_a) + ' VND if we buy from A and ' + str(money_on_b) + ' VND if we buy from B.')
+    # Use if statement to decide choosing A or B for flour supply
     if money_on_a <= money_on_b:
         return total_flour, "A", money_on_a
     return total_flour, "B", money_on_b
 
 
-# define a function to help round up number
+# Define a function to help round up number
 def round_up_to_even(a):
     """
     help to round up to even numbers
     :param a: none
     :return: round up to even a
     """
-    # using math ceiling function and some simple math to help round up numbers to even
+    # Using math ceiling function and some simple math to help round up numbers to even
     return math.ceil(a / 2.) * 2
 
 
-# define flour_amount function
+# Define flour_amount function
 def flour_amount(large_thick, large_thin, medium_thick, medium_thin):
     """
     Calculate the actual amount of flour needed
@@ -59,7 +61,7 @@ def flour_amount(large_thick, large_thin, medium_thick, medium_thin):
     return actual_amount_of_flour
 
 
-# define money_spent_on_a function
+# Define money_spent_on_a function
 def money_spent_on_a(actual_amount_of_flour):
     """
     Calculate the amount of money needed to when buying from A
@@ -67,6 +69,7 @@ def money_spent_on_a(actual_amount_of_flour):
     :return: integer
     """
     money_a = actual_amount_of_flour * 30000
+    # Using if statement to calculate the money needed and minus the discount from A
     if actual_amount_of_flour < 30:
         money_a -= money_a * 0.03
     else:
@@ -82,6 +85,7 @@ def money_spent_on_b(actual_amount_of_flour):
     :return: integer
     """
     money_b = actual_amount_of_flour * 31000
+    # Using if statement to calculate the money needed and minus the discount from B
     if actual_amount_of_flour < 40:
         money_b -= money_b * 0.05
     else:
@@ -89,5 +93,5 @@ def money_spent_on_b(actual_amount_of_flour):
     return money_b
 
 
-# execute function
+# Execute function
 flour_order(10, 30, 10, 50)
